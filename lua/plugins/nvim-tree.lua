9a2fee20e296c8875e024c3512979849fde0fee1
RETRO30 <ssh-off@ya.ru>
2025-08-06 02:47:47 +0500

    add explorer and settings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,40 +1,19 @@
 -- lua/plugins/nvim-tree.lua
-local tree_cb = require("nvim-tree.config").nvim_tree_callback
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
   view = {
     width = 30,
-    side = "left",
-    mappings = {
-      list = {
-        { key = {"l", "<CR>", "o"}, cb = tree_cb("edit") },
-        { key = "h",                cb = tree_cb("close_node") },
-        { key = "v",                cb = tree_cb("vsplit") },
-      },
-    },
   },
   renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-        folder = true,
-        file = true,
-        folder_arrow = true,
-      },
-    },
-  },
-  git = {
-    enable = true,
-    ignore = false,
+    group_empty = true,
   },
   filters = {
-    dotfiles = false,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
+    dotfiles = true,
   },
 })
 
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,25 @@
 --- settings.lua
-local o = vim.opt
-o.number          = true
-o.relativenumber  = true
-o.termguicolors   = true
-o.wrap            = false
+local opt = vim.opt
+
+-- Tabs and indentation
+opt.tabstop = 4                -- width of a hard tabstop
+opt.shiftwidth = 4             -- width for auto-indent
+opt.expandtab = true           -- use spaces instead of tabs
+
+-- Line numbering
+opt.number = true              -- show absolute line numbers
+opt.relativenumber = false     -- no relative numbers
+
+-- Cursor
+opt.cursorline = true          -- highlight the current cursor line
+opt.scrolloff = 8              -- minimal lines to keep above/below cursor
+opt.sidescrolloff = 8          -- minimal columns to keep at sides
+-- Optional: customize cursor appearance (blinking, shape)
+opt.guicursor = {
+  "n-v-c:block",
+  "i-ci-ve:ver25",
+  "r-cr:hor20",
+  "o:hor50",
+  "a:blinkwait700-blinkoff400-blinkon250",
+}
+
